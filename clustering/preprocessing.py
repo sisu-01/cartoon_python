ae851236e0acb222bb661e535b8117fc7af99a8d
sisu-01 <tesv010615@naver.com>
2024-01-24 00:45:27 +0900

    형태소 제외 단어 추가

--- clustering/preprocessing.py
+++ clustering/preprocessing.py
@@ -2,12 +2,18 @@ from mecab import MeCab
 
 mecab = MeCab()
 
+# 제거 단어
+exclude_words = ['MANHWA', 'manhwa', 'MANWHA', 'manwha', '만화', '만와', '망가',
+                 '프롤로그', '에필로그', '마지막화', '完', '후기', '上', '中', '下',
+                 '공지', '휴재', 'bgm', 'BGM', 'ㅇㅎ', '스압']
+
 def main(list):
     # 형태소 목록
     documents = []
     for i in list:
-        a = mecab.nouns(i[1])
-        documents.append(mecab.nouns(i[1]))
+        morphemes = mecab.nouns(i[1])
+        morphemes = [morpheme for morpheme in morphemes if morpheme not in exclude_words]
+        documents.append(morphemes)
     # 형태소 목록을 문장으로 변환
     document_sentences = [' '.join(doc) for doc in documents]
     
