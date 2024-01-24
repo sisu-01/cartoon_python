c95acd3ed5ceb30b5116cb3de5de15eecdfe9321
sisu-01 <tesv010615@naver.com>
2024-01-24 16:24:13 +0900

    주석 제거

--- __main__.py
+++ __main__.py
@@ -1,11 +1,2 @@
 from scraping.scraping import main
-#main()
-
-from clustering.app import main as group
-group('catiff', '카티프')
-
-"""
-할일: 스크래핑 loop 한계 있는거 고치셈
-전체 작가들 싸악 그냥 시리즈 헤응 하셈
-새 작가 하루에 한 번 헤응 하셈
-"""
+main()
