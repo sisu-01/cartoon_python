5421f63957012947f55f1aab3db16532a50c456a
sisu-01 <tesv010615@naver.com>
2024-01-24 15:39:31 +0900

    함수로 쪼갰는데 전역변수 있으면 좋겠다

--- clustering/app.py
+++ clustering/app.py
@@ -3,20 +3,37 @@ from clustering.preprocessing import main as pre_processing
 from clustering.vectorization import main as vectorization
 from clustering.clustering import main as clustering
 
-def reset_series(connection_pool):
-    set_null_result = run_sql(connection_pool, f'UPDATE cartoon SET series_id = null WHERE writer_id = \'{id}\' AND writer_nickname = \'{nickname}\';', None)
+def reset_series(connection_pool, writer_id, writer_nickname):
+    set_null_result = run_sql(connection_pool, f'UPDATE cartoon SET series_id = null WHERE writer_id = \'{writer_id}\' AND writer_nickname = \'{writer_nickname}\';', None)
     if set_null_result:
-        delete_series_result = run_sql(connection_pool, f'DELETE FROM series WHERE writer_id = \'{id}\' AND writer_nickname = \'{nickname}\'', None)
+        delete_series_result = run_sql(connection_pool, f'DELETE FROM series WHERE writer_id = \'{writer_id}\' AND writer_nickname = \'{writer_nickname}\'', None)
         if delete_series_result:
             return True
     return False
-             
-def main(id, nickname):
+
+def set_series(connection_pool, writer_id, writer_nickname, values):
+    insert_sql = (
+        'INSERT INTO series (id, title, writer_id, writer_nickname, count, last_update, average) '
+        'VALUES({}, \'{}\', \'{}\', \'{}\', {}, \'{}\', {})'
+    ).format(
+        values['id'],
+        values['title'],
+        writer_id, writer_nickname,
+        values['count'],
+        values['date'],
+        round(values['recommend'] / values['count'])
+    )
+    insert_result = run_sql(connection_pool, insert_sql, None)
+    if insert_result:
+        update_sql = f"UPDATE cartoon SET series_id = {values['id']} WHERE id IN ({values['list'][:-1]})"
+        run_sql(connection_pool, update_sql, None)
+
+def main(writer_id, writer_nickname):
     connection_pool = create_connection_pool()
     eps = 0.1
 
     # 작가의 만화 목록 불러오기
-    list = run_sql(connection_pool, f'SELECT id, title, date, recommend FROM cartoon WHERE writer_id = \'{id}\' AND writer_nickname = \'{nickname}\' ORDER BY id ASC', None, True)
+    list = run_sql(connection_pool, f'SELECT id, title, date, recommend FROM cartoon WHERE writer_id = \'{writer_id}\' AND writer_nickname = \'{writer_nickname}\' ORDER BY id ASC', None, True)
     # 형태소 분리
     data = pre_processing(list)
     # 벡터화
@@ -24,22 +41,8 @@ def main(id, nickname):
     # 군집화
     result = clustering(eps, vectors, list)
 
-    if reset_series(connection_pool):
+    if reset_series(connection_pool, writer_id, writer_nickname):
         for i in result.keys():
             if i == -1:
                 continue
-            insert_sql = (
-                'INSERT INTO series (id, title, writer_id, writer_nickname, count, last_update, average) '
-                'VALUES({}, \'{}\', \'{}\', \'{}\', {}, \'{}\', {})'
-            ).format(
-                result[i]['id'],
-                result[i]['title'],
-                id, nickname,
-                result[i]['count'],
-                result[i]['date'],
-                round(result[i]['recommend'] / result[i]['count'])
-            )
-            insert_result = run_sql(connection_pool, insert_sql, None)
-            if insert_result:
-                update_sql = f"UPDATE cartoon SET series_id = {result[i]['id']} WHERE id IN ({result[i]['list'][:-1]})"
-                run_sql(connection_pool, update_sql, None)
+            set_series(connection_pool, writer_id, writer_nickname, result[i])
