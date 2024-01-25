5e4d4e778707ed1c1d0ed7a40aa7a34a75a57c90
sisu-01 <tesv010615@naver.com>
2024-01-25 16:52:24 +0900

    당분간 이렇게 두자

--- __main__.py
+++ __main__.py
@@ -1,14 +1,14 @@
-from scraping.scraping import main
-main()
+# from scraping.scraping import main
+# main()
 
 # 작가 목록 싹 돌면서 클러스터링
-# from utils.db import create_connection_pool, run_sql
-# pool = create_connection_pool()
-# list = run_sql(pool, 'select id, nickname from writer where 1=1', None, True)
-# from clustering.app import main as group
-# for i, row in enumerate(list):
-#     print(i, row)
-#     is_a = row[0] == 'a'
-#     is_dd = row[1] == 'ㅇㅇ' or row[1] == '카갤러'
-#     if not (is_a and is_dd):
-#         group(pool, row)
+from utils.db import create_connection_pool, run_sql
+pool = create_connection_pool()
+list = run_sql(pool, 'select id, nickname from writer where 1=1', None, True)
+from clustering.app import main as group
+for i, row in enumerate(list):
+    print(i, row)
+    is_a = row[0] == 'a'
+    is_dd = row[1] == 'ㅇㅇ' or row[1] == '카갤러'
+    if not (is_a and is_dd):
+        group(pool, row)
